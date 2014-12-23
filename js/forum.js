6901e792238bd324410c8edf9a78d6c92317c860
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-23 17:33:51 +0000

    Update forum.js

--- js/forum.js
+++ js/forum.js
@@ -213,7 +213,7 @@ var mainModule = angular.module('app', []).
 		$scope.loginPass = "";
 	
 		$scope.login = function(){
-			if($scope.loginName == "" || $scopeloginPass==""){ $scope.loginFailed = true; return; }
+			if($scope.loginName == "" || $scope.loginPass==""){ $scope.loginFailed = true; return; }
 			
 			var stringifiedPost = JSON.stringify({userName: $scope.loginName, password: $scope.loginPass});
 			$http({method: "GET", url: serverURL + "/posts", data: stringifiedPost}).success(function(data, status){
