c03f59d381f78efca8b5b75994819b8f9660cf37
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2016-12-08 19:34:04 +0000

    Update forum.js

--- js/forum.js
+++ js/forum.js
@@ -236,7 +236,7 @@ var mainModule = angular.module('app', []).
 			if($scope.loginName == "" || $scope.loginPass==""){ $scope.loginFailed = true; return; }
 			
 			var stringifiedPost = JSON.stringify({userName: $scope.loginName, password: $scope.loginPass});
-			$http({method: "POST", url: serverURL + "login", data: stringifiedPost}).success(function(data, status){
+			$http({method: "GET", url: serverURL + "login", data: stringifiedPost}).success(function(data, status){
 				if(data != "false"){
 					//todo: change this later to be assigned on login.
 					$scope.currentUser = $scope.getItemFromID($scope.users, data.id);
